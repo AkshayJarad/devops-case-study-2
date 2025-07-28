a2e45128830c7167afe36d61bdee7e191f083ca2
Akshay Sanjay Jarad <157938695+AkshayJarad@users.noreply.github.com>
2025-07-28 19:16:48 +0530

    Update build_and_push.sh

--- scripts/build_and_push.sh
+++ scripts/build_and_push.sh
@@ -5,10 +5,13 @@ GIT_COMMIT=$(git rev-parse --short HEAD)
 IMAGE="akshayjarad/myapp:$GIT_COMMIT"
 
 echo "[INFO] Building Docker image: $IMAGE"
-docker build -t "$IMAGE" .
+docker build -t "$IMAGE" -t akshayjarad/myapp:latest .
 
-echo "[INFO] Logging in to DockerHub..."
+echo "[INFO] Logging into DockerHub..."
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 
 echo "[INFO] Pushing Docker image to DockerHub..."
 docker push "$IMAGE"
+docker push akshayjarad/myapp:latest
+
+echo "[INFO] Done!"
